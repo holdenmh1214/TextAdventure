ee536f96566914abfa244783a9e7328e13699544
holdenhughes <holdenmh121489@gmail.com>
2015-10-09 11:19:44 -0400

    Refactor code into player class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Created by holdenhughes on 10/8/15.
@@ -6,10 +7,27 @@ public class Game {
     public static void run() throws Exception {
         System.out.println("Welcome to the my text adventure");
 
-        Player player = new Player();
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseArea();
-
+        while (true) {
+            Player player = new Player();
+            player.chooseName();
+            player.chooseWeapon();
+            player.chooseArea();
+        }
+    }
+    static String nextLine() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.startsWith("/")) {
+            System.out.println("Command:");
+            if (s.equals("/Help")) {
+                System.out.println("These are the available commands:");
+                System.out.println("/help ==> Lis available commands");
+            } else if (s.equals("/Exit")) {
+                System.exit(0);
+            }
+            return nextLine();
+        } else {
+            return s;
+        }
     }
 }
